433402b814e4c90a10f4c47a31389436db4d8cf9
Ved Patel <veds28112004@gmail.com>
2025-07-04 18:55:31 -0400

    testing the deployment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: process.env['NG_APP_API_URL'],
+  auth0Domain: process.env['NG_APP_AUTH0_DOMAIN'],
+  auth0ClientId: process.env['NG_APP_AUTH0_CLIENT_ID']
+};
