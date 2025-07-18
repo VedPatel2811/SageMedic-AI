8618fed1e1b46de7764b60291456ffc8544bd8d6
Ved Patel <veds28112004@gmail.com>
2025-07-18 16:36:45 -0400

    Improved the sidebar and the chat

--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output, Input  } from '@angular/core';
+import { Component, EventEmitter, Output, Input, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '@auth0/auth0-angular'; // ensure you have this
 
 @Component({
   selector: 'app-sidebar',
+  standalone: true,
   imports: [CommonModule, MatIconModule],
   templateUrl: './sidebar.html',
 })
 export class Sidebar {
   @Input() sidebarClosed = true;
-
   @Output() sidebarToggle = new EventEmitter<boolean>();
+
+  auth = inject(AuthService);
+  user$ = this.auth.user$;
 
   toggleSidebar() {
     this.sidebarClosed = !this.sidebarClosed;
