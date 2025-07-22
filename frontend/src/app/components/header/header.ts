2b5733ea14a86822ab9d2ea48c8022bca1d48fcd
Ved Patel <veds28112004@gmail.com>
2025-07-22 12:21:18 -0400

    Logout button not working #4 issue fixed

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-
 
 @Component({
   selector: 'app-header',
@@ -14,16 +18,19 @@ import { MatIconModule } from '@angular/material/icon';
 export class Header {
   user: any;
   profileMenuOpen = false;
+
+  @ViewChild('avatarButton') avatarButton!: ElementRef;
+  @ViewChild('profileMenu') profileMenu!: ElementRef;
+
   constructor(public auth: AuthService, private router: Router) {}
 
   ngOnInit() {
-    // subscribe to user profile
-    this.auth.user$.subscribe(profile => {
+    this.auth.user$.subscribe((profile) => {
       this.user = profile;
     });
   }
 
-  goToHome(){
+  goToHome() {
     this.router.navigate(['/']);
   }
 
@@ -35,11 +42,18 @@ export class Header {
     this.auth.logout({ logoutParams: { returnTo: window.location.origin } });
   }
 
-  onAvatarClick(){
-    this.profileMenuOpen = !this.profileMenuOpen;
+  onAvatarClick(event: MouseEvent) {
+    event.stopPropagation();
+    this.profileMenuOpen = true;
   }
 
-  onAvatarBlur(){
-    this.profileMenuOpen=false;
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const clickedInsideAvatar = this.avatarButton?.nativeElement.contains(event.target);
+    const clickedInsideMenu = this.profileMenu?.nativeElement.contains(event.target);
+
+    if (!clickedInsideAvatar && !clickedInsideMenu) {
+      this.profileMenuOpen = false;
+    }
   }
 }
