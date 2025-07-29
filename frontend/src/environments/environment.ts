fbe1253e9ba6f4537d7747eb0fd352b28ace0670
Ved Patel <veds28112004@gmail.com>
2025-07-28 22:56:35 -0400

    Changed the google Cloud url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://symptom-api-8294164700.us-central1.run.app/predict',
+  apiUrl: 'https://symptom-api-59420925485.us-central1.run.app/predict',
   auth0Domain: 'dev-xha8svldp8srggr8.us.auth0.com',
   auth0ClientId: 'nWBm5O96T1VSF76Thu18M8xbEnxlubrn',
 };
